5ca0aae8c8f7947a9f2a4bf79a39d8b32037b161
Nathan Barros <nathansousa0809@gmail.com>
2023-11-12 16:10:06 -0300

    Última tentativa

--- middlewares/multer.js
+++ middlewares/multer.js
@@ -1,9 +1,11 @@
 const multer = require('multer');
 const path = require('path');
 
+const uploadPath = path.join(__dirname, 'uploads');
+
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, "uploads/");
+        cb(null, uploadPath);
     },
     filename: (req, file, cb) => {
         cb(null, Date.now() + path.extname(file.originalname))
